40448a8727bbef670d465b3b18c1c5d3d18fe4cb
Leumig <junmigue7@gmail.com>
2024-04-15 02:51:07 -0300

    hosteo en firebase

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Usuario } from '../../clases/usuario';
+import { Usuario } from '../../models/usuario';
 import { FormsModule } from '@angular/forms';
 
 @Component({
