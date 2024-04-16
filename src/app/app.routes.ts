67431a8731cd2ea192bb7230798c0bafd473c8cd
Leumig <junmigue7@gmail.com>
2024-04-16 19:18:49 -0300

    ruteo en ejercicio 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'bienvenido', pathMatch:'full' },
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ErrorComponent }
+];
+
+// '' Significa sin path. El 'redirectTo' es una redirección, te manda directamente al path 'bienvenido.'
+// El 'pathMatch' es para que funcione correctamente con las '' simples.
+
+// Angular siempre lee cada ruta en orden. Y a la primera que entra, ya deja de analizar las demás.
+// El ** es un comodín. Siempre que llegue ahí, va a entrar.
--- src/app/components/bienvenido/bienvenido.component.ts
+++ src/app/components/bienvenido/bienvenido.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenido',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
+  // Era necesario agregar el ruteo aca, porque los botones del menú los tengo en este componente
   templateUrl: './bienvenido.component.html',
   styleUrl: './bienvenido.component.css'
 })
